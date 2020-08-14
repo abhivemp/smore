92823c4f5086ac896ba35e2e9ea2cd273f47d8e0
abhivemp <abhijitvempati@gmail.com>
2020-08-13 22:30:03 -0400

    fixed views bug with models import

--- smore/search/views.py
+++ smore/search/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import collegeCourse
+from search.models import collegeCourse
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 # Create your views here.
-# core/views.py
+# search/views.py
 def course_view(request):
     ctx = {}
     url_param = request.GET.get("q")
